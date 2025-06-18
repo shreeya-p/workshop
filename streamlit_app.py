c39bb5bab1f50fff996be14e544084c556d6b611
shreeya-p <62971282+shreeya-p@users.noreply.github.com>
2025-06-18 16:15:01 -0400

    Update streamlit_app.py
    changing order

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -46,8 +48,5 @@ if ingredients_list:
         
       st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-#New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
